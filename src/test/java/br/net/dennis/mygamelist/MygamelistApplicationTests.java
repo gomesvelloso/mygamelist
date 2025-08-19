409bcab4e6ff2882a90e42721f989e904c7b1f33
Dennis Henrique <gomesvelloso@gmail.com>
2025-08-18 22:33:05 -0300

    Project created

---
+++ src/test/java/br/net/dennis/mygamelist/MygamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.dennis.mygamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
